9f292bb3beb5d47483e60156c12010c845579ae1
Josh <joshmm16@gmail.com>
2023-11-11 13:12:59 -0800

    Initial commit;

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+import "../css/reset.css";
+import "../css/style.css";
+import "../css/queries.css";
+// import viteLogo from "./vite.svg";
+import { setupCounter } from "./counter.js";
+
+////////////////////////////
+// Variables
+
+////////////////////////////
+// Elements
+
+////////////////////////////
+// Event-listeners
+
+////////////////////////////
+// Functions
