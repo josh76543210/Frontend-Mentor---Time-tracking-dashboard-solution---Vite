2dfd018f422dd90185d8b1abf6a736c105035c83
Josh <joshmm16@gmail.com>
2023-11-15 13:42:48 -0800

    JS - get data from JSON file;

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,43 @@ import { setupCounter } from "./counter.js";
 
 ////////////////////////////
 // Variables
+let reportData = [];
 
 ////////////////////////////
 // Elements
+const dailyBtn = document.querySelector(".main-btn--daily");
+const weeklyBtn = document.querySelector(".main-btn--weekly");
+const monthlyBtn = document.querySelector(".main-btn--monthly");
+const ellipsisEls = document.querySelectorAll(".ellipsis-container");
 
 ////////////////////////////
 // Event-listeners
+// remove hover effect of card when ellipsis are hovered over
+ellipsisEls.forEach((el) =>
+  el.addEventListener("mouseover", (e) => {
+    // get parent card
+    const card = e.target.closest(".card");
+    // style it
+    card.style.backgroundColor = "var(--dark-blue)";
+  })
+);
+// undo removed hover effect when the hover stops
+ellipsisEls.forEach((el) =>
+  el.addEventListener("mouseout", (e) => {
+    // get parent card
+    const card = e.target.closest(".card");
+    // unstyle it
+    card.style.backgroundColor = "";
+  })
+);
 
 ////////////////////////////
 // Functions
+async function getData() {
+  const res = await fetch("../json/data.json");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+reportData = await getData();
+console.log(reportData);
