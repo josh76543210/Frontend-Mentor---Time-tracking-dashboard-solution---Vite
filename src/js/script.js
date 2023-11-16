91f22fcf8a79783471dd05983622e87c73acdf84
Josh <joshmm16@gmail.com>
2023-11-15 17:02:50 -0800

    Cleaned up JavaScript modules;

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 import "../css/reset.css";
 import "../css/style.css";
 import "../css/queries.css";
-// import viteLogo from "./vite.svg";
-import { setupCounter } from "./counter.js";
 
 ////////////////////////////
 // Variables
